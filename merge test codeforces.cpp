545484f071adc4e5845eae82a714ad2bc8aa39fc
kopristupin <67826236+kopristupin@users.noreply.github.com>
2020-07-20 22:40:44 +0300

    Add files via upload

---
+++ merge test codeforces.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+#include<vector>
+#include<algorithm>
+
+int main()
+{
+    int c, n;
+    std::cin >> c;
+    for (int i = 0; i < c; i++) {
+        std::cin >> n;
+        std::vector<int> elements;
+        for (int v = 0; v < 2 * n ; v++) {
+            int y;
+            std::cin >> y;
+            elements.emplace_back(y);
+        }
+        std::vector<int> alreadyExists;
+        std::vector<int>::iterator it = elements.begin();
+
+        
+
+        for (int h = 0; h < elements.size(); h++)
+        {
+       
+            if (std::find(alreadyExists.begin(), alreadyExists.end(), *it) ==alreadyExists.end()) {
+                alreadyExists.emplace_back(*it);
+                std::cout << *it << " ";
+            }
+            it++;
+        }
+
+        std::cout << "\n";
+    }
+}
---
+++ three indexes.cpp
@@ -0,0 +1,61 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<vector>
+#include<iostream>
+#include<algorithm>
+#include<iterator>
+#include<stdio.h>
+
+int main() {
+	int count;
+	std::cin >> count;
+
+
+	for (int i2 = 0; i2 < count; i2++)
+	{
+		int localcount;
+		std::cin >> localcount;
+		int stv[1001];
+		for (int c = 0; c < localcount; c++) {
+
+			std::scanf("%d", &stv[c]);
+
+		}
+
+		bool flag = false;
+
+		int i, j, k;
+		i = 0;
+		while (i < localcount - 2) {
+			j = i + 1;
+			while (j < localcount - 1) {
+				if (stv[j] > stv[i]) {
+					k = j + 1;
+					while (k < localcount) {
+						if (stv[k] < stv[j]) {
+							std::cout << "YES \n";
+							std::cout << i + 1 << " " << j + 1 << " " << k + 1 << "\n";
+							flag = true;
+							break;
+						}
+						k++;
+					}
+				}
+				if (flag)
+					break;
+				j++;
+			}
+			if (flag)
+				break;
+			int n = 1;
+			while (stv[i] < stv[i + n]) {
+				n++;
+				if (n + i == localcount)
+					break;
+			}
+			i += n;
+		}
+		if (flag == false)
+			std::cout << "NO \n";
+	}
+
+}
